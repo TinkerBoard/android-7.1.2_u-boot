bd5a4727c4aaddca5df6d7134f6197db0b51d23a
jamess_huang <Jamess_Huang@asus.com>
2018-02-27 15:08:22 +0800

    added 1 second boot delay for uboot
    Change-Id: Ief401fbd5ea9b4737d2e32f7244e2fe306bac488

--- include/configs/rk32plat.h
+++ include/configs/rk32plat.h
@@ -62,7 +62,7 @@
 #endif /* CONFIG_SECUREBOOT_CRYPTO */
 
 /* mod it to enable console commands.	*/
-#define CONFIG_BOOTDELAY		0
+#define CONFIG_BOOTDELAY		1
 #define CONFIG_DIRECT_LOGO
 
 /* efuse version */
